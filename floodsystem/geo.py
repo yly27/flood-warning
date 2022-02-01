c09ca467c0331dbfa6415f0313552e4f71167b22
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-01 23:53:25 +0000

    geo 1B complete

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,7 +5,47 @@
 geographical data.
 
 """
-from utils import sorted_by_key  # noqa
+from sympy import stationary_points
+from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
+from .station import MonitoringStation
+from .stationdata import build_station_list
+import numpy as np
+from floodsystem.datafetcher import fetch_station_data
+
+
+
+stations = build_station_list
+def stations_by_distance(stations, p):
+    # stations is a list of MonitoringStation objects and p is a coordinate
+    # the function returns the name, town and distance from the coordinate to that particular station
+
+    #this creates a list with the distances, names and town 
+    distances = []
+    names = []
+    towns = []
+
+    for i in stations:
+        distances.append(haversine(p, i.coord)) 
+        names.append(i.name)
+        towns.append(i.town) 
+
+    big_list = list(zip(names,towns,distances))
+    return(sorted_by_key((big_list),2))
+
+    
+
+    
+    
+
+   
+
+
+  
+    
+    
+    
+
+
 
 
