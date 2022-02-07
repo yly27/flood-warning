30e905a87bcca37790969710d2fadcd7168e4a01
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-07 18:51:45 +0000

    1B test

---
+++ test_geo.py
@@ -0,0 +1,32 @@
+import floodsystem.geo as geo
+from floodsystem.stationdata import build_station_list
+from test_station import test_create_monitoring_station
+
+stations = build_station_list()
+tester_station = test_create_monitoring_station()
+
+'''Task 1B Test'''
+def test_stations_by_distance():
+    big_list = geo.stations_by_distance(stations(0,0))
+
+    #Checks if distances have been sorted.
+    for i in big_list:
+        if big_list[i][2] > big_list[i+1][2]:
+            raise ValueError('The list should be in order of increasing distance.')
+    
+    #Checks if distance from (0,0) to tester station (-2,4) is correct
+    assert round(geo.stations_by_distance(tester_station, (0, 0))[0][2]) == 4
+            
+
+'''Task 1C Test'''
+def test_stations_within_radius():
+    
+
+''''Task 1D Test'''
+def test_rivers_with_station():
+
+def test_stations_by_river():
+
+
+
+
