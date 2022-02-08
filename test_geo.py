9f5d7b26865bbfcf2da4177f218c22ec24c07c9f
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-08 13:15:12 +0000

    even more testing

--- test_geo.py
+++ test_geo.py
@@ -34,7 +34,7 @@ def test_stations_within_radius():
 '''Task 1D Test'''
 def test_rivers_with_station():
     assert len(geo.rivers_with_station(stations)) > 0
-    assert geo.rivers_with_station(tester_station)[0] == ['River_Test_2']
+    assert geo.rivers_with_station(tester_station)[0][0] == ['River_Test_2']
 
 
 def test_stations_by_river():
