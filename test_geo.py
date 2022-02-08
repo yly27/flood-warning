5ecbb3a2bf387a34ab4f7e3e91858b657c45fcc2
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-08 11:14:11 +0000

    test 1C (1)

--- test_geo.py
+++ test_geo.py
@@ -1,4 +1,4 @@
-from floodsystem.geo import stations_by_distance
+import floodsystem.geo as geo
 from floodsystem.stationdata import build_station_list
 from test_station import test_create_monitoring_station
 
@@ -8,8 +8,20 @@ tester_station = test_create_monitoring_station()
 
 '''Task 1B Test'''
 def test_stations_by_distance():
-    assert round(stations_by_distance(tester_station, (0, 0))[0][2]) == 497
-    assert len(stations_by_distance(stations, (0, 0))) > 0
+    assert len(geo.stations_by_distance(stations, (0, 0))) >0
+    assert round(geo.stations_by_distance(tester_station, (0,0)) [0][1]) == 497
+
+'''Task 1C Test'''
+def test_stations_within_radius():
+    x = geo.stations_within_radius(stations, (52.2053, 0.1218), 10)
+    assert len(x) == 11
+    assert type(x) == list
+    assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 1)<len(geo.stations_within_radius(stations, (52.2053, 0.1218),2)
+
+
+
+
+
 
 
 
