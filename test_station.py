6531d63ecbc7fc040e98f08e4c2e940dcae95b72
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-08 13:31:17 +0000

    added comment

--- test_station.py
+++ test_station.py
@@ -7,6 +7,7 @@ from floodsystem.station import MonitoringStation
 from test_geo import tester_station
 
 
+'''Task 1F Test'''
 def test_typical_range_consistent():
     assert tester_station[0].typical_range_consistent() == False
     assert tester_station[1].typical_range_consistent() == True
