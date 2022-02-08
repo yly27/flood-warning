7073f4f764d77830a68752176a0d76776a55e47d
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-08 11:20:14 +0000

    test 1C (3)

--- test_geo.py
+++ test_geo.py
@@ -9,11 +9,12 @@ tester_station = test_create_monitoring_station()
 '''Task 1B Test'''
 def test_stations_by_distance():
     assert len(geo.stations_by_distance(stations, (0, 0))) >0
-    assert round(geo.stations_by_distance(tester_station, (0,0)) [0][1]) == 497
+    assert round(geo.stations_by_distance(tester_station, (0, 0))[0][2]) == 497
 
 '''Task 1C Test'''
 def test_stations_within_radius():
-    x = geo.stations_within_radius(stations, (52.2053, 0.1218), 10)
-    assert len(x) == 11
-    assert type(x) == list
-    assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 1))<len(geo.stations_within_radius(stations, (52.2053, 0.1218),2))
+    assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 0)) == 0
+    assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 10)) > 0
+    assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 5)) > len(geo.stations_within_radius(stations, (52.2053, 0.1218), 1))
+
+    
