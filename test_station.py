b3ddbdb6c82ab7d2374703bd4a1fc55d168a95eb
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-08 13:27:46 +0000

    trying 1F test

--- test_station.py
+++ test_station.py
@@ -4,25 +4,9 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
+from test_geo import tester_station
 
 
-def test_create_monitoring_station():
-
-    # Create a station
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2, 4)
-    trange = (-2.3, 3.4445)
-    river = "River X"
-    town = "My Town"
-    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
-    
-
-    assert s.station_id == s_id
-    assert s.measure_id == m_id
-    assert s.name == label
-    assert s.coord == coord
-    assert s.typical_range == trange
-    assert s.river == river
-    assert s.town == town
+def test_typical_range_consistent():
+    assert tester_station[0].typical_range_consistent() == False
+    assert tester_station[1].typical_range_consistent() == True
