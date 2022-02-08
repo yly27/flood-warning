0c6fc4f3420b24922f69123f06028c953217b020
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-08 12:48:37 +0000

    test 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -23,7 +23,7 @@ def stations_by_distance(stations, p):
     names = []
     towns = []
 
-    for i in list(stations):
+    for i in stations:
         distances.append(haversine(p, i.coord)) 
         names.append(i.name)
         towns.append(i.town) 
--- test_geo.py
+++ test_geo.py
@@ -6,14 +6,22 @@ from test_station import test_create_monitoring_station
 stations = build_station_list()
 tester_station = test_create_monitoring_station()
 
-'''Task 1B Test'''
-def test_stations_by_distance():
-    assert len(geo.stations_by_distance(stations, (0, 0))) >0
-    assert round(geo.stations_by_distance(tester_station, (0, 0))[0][2]) == 497
 
 '''Task 1C Test'''
 def test_stations_within_radius():
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 0)) == 0
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 10)) > 0
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 5)) > len(geo.stations_within_radius(stations, (52.2053, 0.1218), 1))
+
+
+'''Task 1D Test'''
+def test_rivers_with_station():
+    assert len(geo.rivers_with_station(stations)) > 0
+    assert geo.rivers_with_station(tester_station) == ['River X']
+
+def test_stations_by_river():
+    assert type(geo.stations_by_river) == dict
+    assert len(geo.stations_by_river(stations)['River Cam']) > 5
+
+
 
