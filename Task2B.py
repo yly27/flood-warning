b85decaf56145ed0e6d468c7ba841b58f960d7ba
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-10 23:11:34 +0000

    task 2B (4)

--- Task2B.py
+++ Task2B.py
@@ -1,16 +1,17 @@
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import station_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import MonitoringStation
+
 
 def run():
     """Requirements for Task 2B"""
 
     stations = build_station_list()
 
-    # Update latest level data for all stations
     update_water_levels(stations)
 
     stations_over_tol = station_level_over_threshold(stations, 0.8)
-    print(stations_over_tol)
+    print(stations_over_tol)  
 
 
 
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,25 +6,13 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 
 stations = build_station_list()
 def station_level_over_threshold(stations, tol):
-
+    big_list = []
     #Locates stations with relative water level is over tol and adds to a list
     for i in stations:
         if i.relative_water_level > tol:
-            print('Bigger than tol')
+            big_list.append(i,i.relative_water_level)
 
 
-
-
-
-
-  #  distances = []
-   # names = []
-    #towns = []
-
-    #for i in stations:
-     #   distances.append(haversine(p, i.coord)) 
-      #  names.append(i.name)
-       # towns.append(i.town) 
-
-    #big_list = list(zip(names,towns,distances))
-    #return(sorted_by_key((big_list),2))
+    big_list.sort(reverse=True)
+    return big_list
+  
