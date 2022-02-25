1a3499999654123147c7f23d08cf570cb5209a50
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-25 16:57:38 +0000

    starting 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,5 +1,6 @@
 from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list, update_water_levels
+from .utils import sorted_by_key 
 
 
 '''Task 2B part b'''
@@ -16,3 +17,22 @@ def station_level_over_threshold(stations, tol):
     big_list.sort(reverse=True)
     return big_list
   
+
+'''Task 2C'''
+def stations_highest_rel_level(stations, N):
+    station_names = [], relative_levels = []
+    for i in stations:
+        if i.relative_water_level() == False:
+            pass
+        else:
+            #list of all stations and their relative water level
+            station_names.append(i.name)
+            relative_levels.append(i.relative_water_level)
+
+    #combining the two lists into a list of tuples
+    station_biglist = list(zip(station_names, relative_levels))
+
+    #sort list biggest to smallest
+    station_biglist = sorted_by_key(station_biglist, 1, reverse = False)  
+
+    return station_biglist[:N]
