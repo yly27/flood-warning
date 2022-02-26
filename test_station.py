d240aceed6ee8cdda6783d60f6e8b635e642765f
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-26 23:49:32 +0000

    test relative water level 1

--- test_station.py
+++ test_station.py
@@ -4,6 +4,12 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+import random
+
+stations = build_station_list()
+
+#Import test data
 from test_geo import tester_station
 
 
@@ -11,3 +17,11 @@ from test_geo import tester_station
 def test_typical_range_consistent():
     assert tester_station[0].typical_range_consistent() == False
     assert tester_station[1].typical_range_consistent() == True
+
+'''Task 2B Test (Part A)'''
+def test_relative_water_level():
+    #Typical range for test_station_2 is 10 to 20
+    #Check that the relative water level calculation is correct if the current water level is 15
+    assert tester_station[1].relative_water_level() == 0.5
+    
+    assert type(stations[random.randint(len(stations))]) == float 
