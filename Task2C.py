476140a65e173b83cda44e23d788a6e488374f46
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 11:46:28 +0000

    2C some code wasn't in run function

--- Task2C.py
+++ Task2C.py
@@ -5,13 +5,13 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 def run():
     """Requirements for Task 2C"""
 
-#build list of stations and update
-stations = build_station_list()
-update_water_levels(stations)
+    #build list of stations and update
+    stations = build_station_list()
+    update_water_levels(stations)
 
-#find top 10 most at risk
-for i in stations_highest_rel_level(stations, 10):
-    print("{}  {}".format(i[0], i[1]))
+    #find top 10 most at risk
+    for i in stations_highest_rel_level(stations, 10):
+        print("{}  {}".format(i[0], i[1]))
 
 if __name__ == "__main__":
     print("*** Task 1F: CUED Part IA Flood Warning System ***")
