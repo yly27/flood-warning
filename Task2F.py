6fb536a8893baa98c0bd9914358c7afff68bbd9a
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 15:58:04 +0000

    2f finished?

--- Task2F.py
+++ Task2F.py
@@ -1,10 +1,8 @@
 import datetime
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-from floodsystem.analysis import polyfit
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.plot import plot_water_level_with_fit
 
 def run():
     """Requirements for Task 2F"""
@@ -15,8 +13,14 @@ def run():
 
     high_five = stations_highest_rel_level(stations, 5)
 
+    for i in high_five:
+        station = i[0]
+
+        dt = 2
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_level_with_fit(station, dates, levels, 4) 
 
 
 if __name__ == "__main__":
-    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
