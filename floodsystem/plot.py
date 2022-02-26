6df3e0c686caaa0be0bce40387af2c44d04b290e
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 15:58:43 +0000

    adding labels to plot

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -34,14 +34,16 @@ def plot_water_level_with_fit(station, dates, levels, p):
         dates = matplotlib.dates.date2num(dates) - d0
         x = np.linspace(dates[0],dates[-1],30)
         
-        plt.plot(dates, levels)
+        plt.plot(dates, levels, '.')
         plt.plot(x, poly(x), label = "Best-fit polynomial")
-        plt.plot(x, np.linspace(station.typical_range[0],station.typical_range[0],30), label = "Typical low range")
-        plt.plot(x, np.linspace(station.typical_range[1],station.typical_range[1],30), label = "Typical high range")
-        
-        plt.xlabel("Number of days")
-        plt.ylabel("Water level (m)")
-        plt.title(station.name, "water level for last", round(abs(dates[-1]),1), "days")
+        plt.plot(x, np.linspace(station.typical_range[0],station.typical_range[0],30), '-b', label = "Typical relative low value")
+        plt.plot(x, np.linspace(station.typical_range[1],station.typical_range[1],30), '-g', label = "Typical relative high value")
+        plt.legend(loc = 'best')
+
+        plt.xlabel("Number of days ago")
+        plt.ylabel("Relative water level")
+        plt.title(station.name)
+        plt.tight_layout()
 
     plt.show()
 
