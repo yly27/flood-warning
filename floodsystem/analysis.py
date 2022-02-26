0cb731c9bf21b353205a717dbe0642c1daf78ae2
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 23:57:15 +0000

    it wont run

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,11 +1,14 @@
+from re import X
 from unicodedata import name
 import numpy as np
 import matplotlib
+from datetime import datetime, timedelta
 from .stationdata import build_station_list
 from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 from floodsystem.stationdata import update_water_levels
 import datetime
 from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_highest_rel_level
 
 def polyfit(dates, levels, p):
     # Create set of data points
@@ -42,16 +45,27 @@ def risk_analysis(stations):
         
         current_rel_water_levels.append(i.relative_water_level())
 
-    #Take the predicted water levels for the next day 
-        # Fetch data over past 2 days
+    #Takes too long to check every station so just check top 50
+    
+    to_check = []
+
+    for i in consistent_stations: 
+        to_check.append(stations_highest_rel_level(stations, 40))
+        
+        
+    rel_predicted = []   
+   
+    for i in to_check:
         dt = 2
         dates, levels = fetch_measure_levels(i.measure_id, dt=datetime.timedelta(days=dt))
+        x = matplotlib.dates.date2num(dates)
         poly, d0 = polyfit(dates, levels, 4)
-        day_today = max(matplotlib.dates.dates2num(dates) - d0)
+        day_today = max(x - d0)
 
         #predict tomorrow
         predicted = poly(day_today + 1)
-        rel_predicted = ((predicted - i.typical_range[0])/i.typical_range[1] - i.typical_range[0])
+        calculation =((predicted - i.typical_range[0])/(i.typical_range[1] - i.typical_range[0]))
+        rel_predicted.append(calculation)
 
-        return rel_predicted[:5]
+    return rel_predicted[:5]
 
