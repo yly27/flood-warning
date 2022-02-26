2603baaa90760b812d42f3fb8405fbab2eb2c159
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 14:37:28 +0000

    graph no formatting

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import matplotlib
+from floodsystem.analysis import polyfit
+from floodsystem.flood import stations_highest_rel_level
+import numpy as np
 
 def plot_water_levels(station, dates, levels):
 
@@ -15,4 +18,25 @@ def plot_water_levels(station, dates, levels):
 
     plt.tight_layout
 
-    
+
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    if dates == None or levels == None:
+        pass
+
+    else:
+        poly, d0 = polyfit(dates, levels, p)
+        dates = matplotlib.dates.date2num(dates) - d0
+        x = np.linspace(dates[0],dates[-1],30)
+        
+        plt.plot(dates, levels)
+        plt.plot(x, poly(x), label = "Best-fit polynomial")
+        plt.plot(x, np.linspace(station.typical_range[0],station.typical_range[0],30), label = "Typical low range")
+        plt.plot(x, np.linspace(station.typical_range[1],station.typical_range[1],30), label = "Typical high range")
+        
+        plt.xlabel("Number of days")
+        plt.ylabel("Water level (m)")
+        plt.title(station.name, "water level for last", round(abs(dates[-1]),1), "days")
+
+    plt.show()
+
