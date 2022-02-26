33a5e48128690c9047b267e94c071bab6dc26a64
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-26 14:37:09 +0000

    tidying

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,19 +1,18 @@
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
 
 def polyfit(dates, levels, p):
     # Create set of data points
     x = matplotlib.dates.date2num(dates)
     y = levels
 
+    #The amount we shifted the dates by
+    d0 = x[0]
+
     # Find coefficients of best-fit polynomial f(x) of degree p. We use x-x[0] to shift the date values
     p_coeff = np.polyfit(x-x[0], y, p)
 
     # Convert coefficient into a polynomial that can be evaluated
     poly = np.poly1d(p_coeff)
-
-    #The amount we shifted the dates by
-    d0 = matplotlib.dates.date2num(dates[0])
 
     return poly, d0
