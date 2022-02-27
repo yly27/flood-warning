cc5a1a0d40fd40acabd501f1c93e27cfd38b1ab4
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-27 11:18:23 +0000

    commit

--- test_flood.py
+++ test_flood.py
@@ -3,8 +3,9 @@ from test_geo import tester_station
 from floodsystem.flood import stations_level_over_threshold
 
 '''Task 2B Test (Part 2)'''
-tester_station[1].latest_level = 20
-test_list = stations_level_over_threshold(tester_station, 0.5)
+def test_stations_level_over_threshold():
+    tester_station[1].latest_level = 20
+    test_list = stations_level_over_threshold(tester_station, 0.5)
 
-for i in range (len(test_list)):
-    assert test_list[i][1] > 0.5
+    for i in range (len(test_list)):
+        assert test_list[i][1] > 0.5
