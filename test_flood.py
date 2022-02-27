d9d65a42e53684384cee2711d2aa167b22e63359
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-27 11:24:27 +0000

    commit agan

--- test_flood.py
+++ test_flood.py
@@ -1,11 +1,16 @@
 #Import test data
+from floodsystem.stationdata import build_station_list
 from test_geo import tester_station
 from floodsystem.flood import stations_level_over_threshold
+from floodsystem.station import MonitoringStation
+
+stations = build_station_list
 
 '''Task 2B Test (Part 2)'''
 def test_stations_level_over_threshold():
-    tester_station[1].latest_level = 20
-    test_list = stations_level_over_threshold(tester_station, 0.5)
+    test_list = stations_level_over_threshold(stations, 0.5)
 
     for i in range (len(test_list)):
         assert test_list[i][1] > 0.5
+        assert test_list[i][1] != None
+        assert test_list[i+1][1] < test_list[i][1]
