7029c2946485bf7a6430b6b7e34ff58efd6fc8b0
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-27 11:07:32 +0000

    commit

--- test_station.py
+++ test_station.py
@@ -5,9 +5,6 @@
 
 from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
-import random
-
-stations = build_station_list()
 
 #Import test data
 from test_geo import tester_station
@@ -18,11 +15,11 @@ def test_typical_range_consistent():
     assert tester_station[0].typical_range_consistent() == False
     assert tester_station[1].typical_range_consistent() == True
 
-'''Task 2B Test (Part A)'''
+'''Task 2B Test (Part 1)'''
 def test_relative_water_level():
     #Typical range for test_station_2 is 10 to 20
     #Check that the relative water level calculation is correct if the current water level is 20
     tester_station[1].latest_level = 20
     assert tester_station[1].relative_water_level() == 1
     assert tester_station[0].relative_water_level() == None
-    
+
