23ed1d403e100f64e8737561e95a044fb22c5484
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-27 20:05:41 +0000

    polyfit test - types

--- test_analysis.py
+++ test_analysis.py
@@ -1,2 +1,27 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+import random
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.analysis import polyfit
+import numpy
+import datetime
+
 def test_risk_analysis():
     print('DUNNO')
+
+
+
+def test_polyfit():
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    #random station
+    n = random.randint(1,50)
+    test_station = stations[n]
+
+    dates, levels = fetch_measure_levels(test_station.measure_id, dt=datetime.timedelta(days=4))
+    poly, d0 = polyfit(dates, levels, 5)
+
+
+    assert len(poly) != 0
+    assert type(poly) == numpy.poly1d
+    
