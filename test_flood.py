2d994a0bce3df0eac63dd5acda0a2211a3e86c2e
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-27 11:29:00 +0000

    commit

--- test_flood.py
+++ test_flood.py
@@ -8,9 +8,7 @@ stations = build_station_list
 
 '''Task 2B Test (Part 2)'''
 def test_stations_level_over_threshold():
-    test_list = stations_level_over_threshold(stations, 0.5)
-
-    for i in range (len(test_list)):
-        assert test_list[i][1] > 0.5
-        assert test_list[i][1] != None
-        assert test_list[i+1][1] < test_list[i][1]
+    for i in stations_level_over_threshold(stations, 0.8):
+        assert i[1] > 0.5
+        assert i[1] != None
+        assert i[1] > (i+1)[1]
