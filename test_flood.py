2850a9bd71e73060cfc06e773c3c120b7c62885c
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-27 12:07:45 +0000

    commit

--- test_flood.py
+++ test_flood.py
@@ -1,14 +1,38 @@
-#Import test data
-from floodsystem.stationdata import build_station_list
-from test_geo import tester_station
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.station import MonitoringStation
 
-stations = build_station_list
+tester_stations = [MonitoringStation(
+        station_id=1,
+        measure_id=15,
+        label='Station 1',
+        coord=(float(0.5), float(1)),
+        typical_range=(1.2, 1.5),
+        river='Test River',
+        town='Town 1'),
+        MonitoringStation(
+        station_id=2,
+        measure_id=20,
+        label='Station 2',
+        coord=(float(10), float(50)),
+        typical_range=(2, 5),
+        river='Test River',
+        town='Town 2'),
+        MonitoringStation(
+        station_id=3,
+        measure_id=50,
+        label='Station 3',
+        coord=(float(25), float(35)),
+        typical_range=(1, 19),
+        river='Test River',
+        town='Town 3')]
+
 
 '''Task 2B Test (Part 2)'''
 def test_stations_level_over_threshold():
-    for i in stations_level_over_threshold(stations, 0.8):
-        assert i[1] > 0.5
-        assert i[1] != None
-        assert i[1] > (i+1)[1]
+    #Assign each station a current water level
+    tester_stations[0].latest_level = 1.5
+    tester_stations[1].latest_level = 10
+    tester_stations[2].latest_level = 25
+
+    test_list = stations_level_over_threshold(tester_stations,0.5)
+    assert test_list[0][1] > test_list[1][1]
