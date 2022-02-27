cbe77a2854c5573202a80dd1a6fe8ef8551e1b55
Yen Li Yeap <yly27@cam.ac.uk>
2022-02-27 13:41:50 +0000

    this works maybe

--- Task2G.py
+++ Task2G.py
@@ -1,15 +1,20 @@
-from floodsystem.analysis import risk_analysis
-from floodsystem.stationdata import build_station_list
+from floodsystem.analysis import risk_analysis, risk_rater
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 
 def run():
     """Requirements for Task 2G"""
     
     stations = build_station_list()
-    #for station in risk_analysis(stations):
-    #    print("station {} risk level {}".format((station[0]).name, station[1]))
+    update_water_levels(stations)
 
-    print(risk_analysis(stations))
+    liszt = risk_analysis(stations)
+    print(liszt)
+    
+    final_list = sorted(liszt.items(), key = lambda x: x[1], reverse = True)
+    
+    for i in final_list:
+        print("Town: {} Risk level: {} Risk rating: {}".format(i[0], i[1], risk_rater(i[1])) )
       
 
 if __name__ == "__main__":
