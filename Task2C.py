a50649d50383b9135cd5c807c53902731367ab3f
Hirushi Thenuwara <ht446@cam.ac.uk>
2022-02-28 11:08:20 +0000

    commit

--- Task2C.py
+++ Task2C.py
@@ -10,10 +10,12 @@ def run():
     update_water_levels(stations)
 
     high_ten = stations_highest_rel_level(stations, 10)
-    
+   
     #find top 10 most at risk
     for station in high_ten:
         print("{} {}".format((station[0]).name, station[1]))
+
+    print('Stations with a relative water level above 100 have been removed')
 
 if __name__ == "__main__":
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
